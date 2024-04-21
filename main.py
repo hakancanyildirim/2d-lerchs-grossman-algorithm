c9b6550817777fb0ab894e993ecf5e6feee28ac4
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-04-21 15:26:58 +0300

    Initialize net profit calculation.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+block_dimension_x = input("Enter the number of blocks for the X dimension:")
+block_dimension_y = input("Enter the number of blocks for the Y dimension:")
+
+costs = float(input("Costs in dollars: "))
+revenues = float(input("Revenues in dollars: "))
+
+profit = costs - revenues
+
+print("Here is your net profit: " + "$" + str(profit))
